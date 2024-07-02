dd2ec51f7f0e5f54999bfab4f0ebcc5dbd2682ec
Desiree Chukwuji <queendesi06@gmail.com>
2024-07-02 09:04:31 +0100

    updated styling

--- script.js
+++ script.js
@@ -1,26 +1,42 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const mobileMenu = document.getElementById('mobile-menu');
-    const navList = document.querySelector('.nav-list');
+document.addEventListener("DOMContentLoaded", function () {
+  const mobileMenu = document.getElementById("mobile-menu");
+  const navList = document.querySelector(".nav-list");
 
-    mobileMenu.addEventListener('click', function() {
-        navList.classList.toggle('active');
-    });
+  mobileMenu.addEventListener("click", function () {
+    navList.classList.toggle("active");
+  });
 });
 
-    
-    function updateTimeAndDay() {
-        const utcTimeElement = document.getElementById("utcTime");
-        const dayOfWeekElement = document.getElementById("dayOfWeek");
+const image = document.querySelector("img");
 
-        const now = new Date();
-        const utcTime = now.toISOString().split('T')[1].split('.')[0];
-        const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-        const currentDay = days[now.getUTCDay()];
+image.addEventListener("load", (event) => {
+  const { naturalWidth, naturalHeight, width, height } = image;
+  output.textContent = `
+Natural size: ${naturalWidth} x ${naturalHeight} pixels
+Displayed size: ${width} x ${height} pixels
+`;
+});
 
-        utcTimeElement.textContent = utcTime;
-        dayOfWeekElement.textContent = currentDay;
-    }
+function updateTimeAndDay() {
+  const utcTimeElement = document.getElementById("utcTime");
+  const dayOfWeekElement = document.getElementById("dayOfWeek");
 
-    
-    updateTimeAndDay();
-    setInterval(updateTimeAndDay, 1000);
+  const now = new Date();
+  const utcTime = now.toISOString().split("T")[1].split(".")[0];
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const currentDay = days[now.getUTCDay()];
+
+  utcTimeElement.textContent = utcTime;
+  dayOfWeekElement.textContent = currentDay;
+}
+
+updateTimeAndDay();
+setInterval(updateTimeAndDay, 1000);
